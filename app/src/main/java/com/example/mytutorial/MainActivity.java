62e29df5ece38f1147de09be6173d0ebf4ff739d
andrealondero <a.londero@itsrizzoli.it>
2019-02-18 15:15:01 +0100

    aggiornamento 18_02 funzionante

--- app/src/main/java/com/example/mytutorial/MainActivity.java
+++ app/src/main/java/com/example/mytutorial/MainActivity.java
@@ -5,40 +5,17 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
-import android.widget.Button;
-import android.widget.CheckBox;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private View view;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-  //      Button b = (Button) findViewById(R.id.done);
- /*       CheckBox ce = (CheckBox) findViewById(R.id.elegantaccess);
 
-        b.setVisibility(b.INVISIBLE);
-
-        b.setOnClickListener(new View.OnClickListener()
-    {
-        if (cause) {
-            b.setVisibility( View.VISIBLE );
-        }
-
-        if (!cause) {
-            b.setVisibility( View.GONE );
-        }
-    } */
-
-    public void onClick(View view) {
+    public void Onclick(View view) {
         startActivity(new Intent(MainActivity.this, SecondActivity.class));
-
-  //     b.setVisibility( View.VISIBLE );
     }
 }
 
